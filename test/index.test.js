8ccbd8cd7b7e98bd78ca4662bfd509bbe14d4c99
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-01-15 17:49:14 +1100

    add tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,44 @@
 /* eslint-env node, mocha */
 const chai = require('chai');
 const sinon = require('sinon');
+const path = require('path');
+const dependencyTree = require('dependency-tree');
 const { getInstalledPathSync } = require('get-installed-path');
-
 const InjectDependencies = require('../index');
 
 const expect = chai.expect;
 
-chai.use(require('chai-as-promised'));
 chai.use(require('sinon-chai'));
 
 const serverlessPath = getInstalledPathSync('serverless', { local: true });
 const AwsProvider = require(`${serverlessPath}/lib/plugins/aws/provider/awsProvider`); // eslint-disable-line
 const Serverless = require(`${serverlessPath}/lib/Serverless`); // eslint-disable-line
 
+
+const dummyDependencyLists = {
+  'path/to/fileA.js': [
+    '/Users/name/project/node_modules/sax/lib/sax.js',
+    '/Users/name/project/node_modules/xml2js/lib/defaults.js',
+    '/Users/name/project/node_modules/xml2js/lib/builder.js',
+    '/Users/name/project/node_modules/xml2js/lib/bom.js',
+    '/Users/name/project/node_modules/xml2js/lib/processors.js',
+    '/Users/name/project/node_modules/xml2js/lib/parser.js',
+    '/Users/name/project/node_modules/xml2js/lib/xml2js.js',
+  ],
+  'path/to/fileB.js': [
+    '/Users/name/project/node_modules/sax/lib/sax.js',
+    '/Users/name/project/node_modules/something/index.js',
+  ],
+};
+
 describe('Inject Dependencies', () => {
   let serverless;
   let options;
   let sandbox;
+  let plugin;
 
   before(() => {
-    sandbox = sinon.sandbox.create();
+    sandbox = sinon.createSandbox();
   });
 
   beforeEach(() => {
@@ -28,9 +46,17 @@ describe('Inject Dependencies', () => {
       stage: 'myStage',
       region: 'us-east-1',
     };
+    sandbox.stub(process, 'cwd').returns('/Users/name/project');
+    sandbox.stub(dependencyTree, 'toList').callsFake((obj) => {
+      const key = path.relative(process.cwd(), obj.filename);
+      return dummyDependencyLists[key];
+    });
     serverless = new Serverless(options);
     serverless.cli = new serverless.classes.CLI(serverless);
     serverless.service.service = 'myService';
+    serverless.config.servicePath = '';
+    serverless.service.package = {};
+    serverless.service.functions = {};
     serverless.setProvider('aws', new AwsProvider(serverless, options));
   });
 
@@ -39,13 +65,73 @@ describe('Inject Dependencies', () => {
   });
 
   describe('constructor', () => {
-
+    it('returns InjectDependenciesPlugin object with hook `before:package:initialize`', () => {
+      plugin = new InjectDependencies(serverless);
+      expect(plugin.hooks).to.have.all.keys('before:package:initialize');
+    });
   });
 
-  describe('before:package:initialize', () => {
-    it('should return', () => {
-      const plugin = new InjectDependencies(serverless);
-      return expect(plugin.hooks['before:package:initialize']()).to.equal(undefined);
+  describe('hook before:package:initialize', () => {
+    it('returns synchronously', () => {
+      plugin = new InjectDependencies(serverless);
+      expect(plugin.hooks['before:package:initialize']()).to.equal(undefined);
+    });
+
+    it('sets service to automatically exclude all dependencies', () => {
+      plugin = new InjectDependencies(serverless);
+      plugin.hooks['before:package:initialize']();
+      expect(serverless.service.package.exclude).to.deep.equal(['./**']);
+    });
+
+    it('adds detected dependencies to existing includes', () => {
+      serverless.service.package.include = ['already/here'];
+      serverless.service.functions = {
+        post: {
+          handler: 'path/to/fileB.post',
+        },
+      };
+      plugin = new InjectDependencies(serverless);
+      plugin.hooks['before:package:initialize']();
+      const expected = ['already/here', 'node_modules/sax/**', 'node_modules/something/**'];
+      expect(serverless.service.package.include).to.deep.equal(expected);
+    });
+
+    it('adds only unique dependencies', () => {
+      serverless.service.functions = {
+        post: {
+          handler: 'path/to/fileB.post',
+        },
+        get: {
+          handler: 'path/to/fileA.get',
+        },
+      };
+      plugin = new InjectDependencies(serverless);
+      plugin.hooks['before:package:initialize']();
+      const expected = ['node_modules/sax/**', 'node_modules/something/**', 'node_modules/xml2js/**'];
+      expect(serverless.service.package.include).to.deep.equal(expected);
+    });
+
+    it('only analyses files once, even if referenced multiple times', () => {
+      serverless.service.package.include = ['already/here'];
+      serverless.service.functions = {
+        get: {
+          handler: 'path/to/fileA.get',
+        },
+        del: {
+          handler: 'path/to/fileA.del',
+        },
+        func: {
+          handler: 'path/to/fileA.func',
+        },
+      };
+      plugin = new InjectDependencies(serverless);
+      plugin.hooks['before:package:initialize']();
+      const file = '/Users/name/project/path/to/fileA';
+      expect(dependencyTree.toList.calledOnce).to.equal(true);
+      expect(dependencyTree.toList.calledWithExactly({
+        filename: `${file}.js`,
+        directory: path.dirname(file),
+      })).to.equal(true);
     });
   });
 });
